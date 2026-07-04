d97165e246b71b881c6fbbf29f58d4b6893b1122
Toshiki Miyagawa <toshi402@gmail.com>
2026-07-05 01:18:28 +0900

    Remove unused Planck layers

--- keyboards/planck/rev6_drop/keymaps/planck_keymap/config.h
+++ keyboards/planck/rev6_drop/keymaps/planck_keymap/config.h
@@ -21,7 +21,7 @@
 // #define STARTUP_SONG SONG(NO_SOUND)
 
 #    define DEFAULT_LAYER_SONGS \
-        { SONG(QWERTY_SOUND), SONG(COLEMAK_SOUND), SONG(DVORAK_SOUND) }
+        { SONG(QWERTY_SOUND) }
 #endif
 
 /*
